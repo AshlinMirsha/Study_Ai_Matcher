b55402883211b08059283d5a100c0033e8d667c8
AshlinMirsha <ashlinmirsha@karunya.edu.in>
2026-07-11 16:16:33 +0530

    Updated create_db.py

--- backend/create_db.py
+++ backend/create_db.py
@@ -2,7 +2,7 @@ import psycopg
 from psycopg import errors
 
 try:
-    with psycopg.connect("dbname=postgres user=postgres password=postgres host=localhost port=5432", autocommit=True) as conn:
+    with psycopg.connect("dbname=postgres user=postgres password=ashlin  host=localhost port=5432", autocommit=True) as conn:
         with conn.cursor() as cursor:
             cursor.execute("CREATE DATABASE study_ai_matcher")
             print("Database 'study_ai_matcher' created successfully.")
